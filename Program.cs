c29dc7d913f1b2bb0d332fa52224548e2b006d27
GachiSolid <miftahovsalavat@gmail.com>
2021-03-30 17:50:43 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SubsidyCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Tariff tariff = new Tariff
+            {
+                HouseId = 1,
+                PeriodBegin = new DateTime(2021, 1, 1),
+                PeriodEnd = new DateTime(2022, 1, 1),
+                ServiceId = 5,
+                Value = 10
+            };
+            Volume volume = new Volume
+            {
+                HouseId = 1,
+                Month = new DateTime(2020, 4, 1),
+                ServiceId = 5,
+                Value = 100
+            };
+
+            SubsidyCalculation sub = new SubsidyCalculation();
+            Charge charge = sub.CalculateSubsidy(volume, tariff);
+
+            Console.WriteLine(charge.Value);
+        }
+    }
+}
---
+++ SubsidyCalculation.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SubsidyCalculation
+{
+    class SubsidyCalculation : ISubsidyCalculation
+    {
+        public event EventHandler<string> OnNotify = Notify;
+        public event EventHandler<Tuple<string, Exception>> OnException = Exception;
+
+        void Check(Volume volume, Tariff tariff)
+        {
+            if (volume.HouseId != tariff.HouseId)
+            {
+                Exception e = new ArgumentException("Дом");
+                OnException.Invoke(this, new Tuple<string, Exception>("Идентификаторы домов не совпадают", e));
+                throw e;
+            }
+            if (volume.ServiceId != tariff.ServiceId)
+            {
+                Exception e = new ArgumentException("Услуга");
+                OnException.Invoke(this, new Tuple<string, Exception>("Идентификаторы услуги не совпадают", e));
+                throw e;
+            }
+            if(!(volume.Month >= tariff.PeriodBegin && volume.Month <= tariff.PeriodEnd))
+            {
+                Exception e = new ArgumentOutOfRangeException("Месяц");
+                OnException.Invoke(this, new Tuple<string, Exception>("Месяц не входит в тариф", e));
+                throw e;
+            }
+            if (tariff.Value <= 0)
+            {
+                Exception e = new ArgumentException("Тариф");
+                OnException.Invoke(this, new Tuple<string, Exception>("Не допускаются нулевые или отрицательные значения тарифа", e));
+                throw e;
+            }
+            if (volume.Value < 0)
+            {
+                Exception e = new ArgumentException("Объем");
+                OnException.Invoke(this, new Tuple<string, Exception>("Не допускаются отрицательные значения объема", e));
+                throw e;
+            }
+        }
+
+        public Charge CalculateSubsidy(Volume volumes, Tariff tariff)
+        {
+            try
+            {
+                Check(volumes, tariff);
+                OnNotify.Invoke(this, $"Расчет начат в {DateTime.Now:T}");
+                Charge charge = new Charge
+                {
+                    HouseId = volumes.HouseId,
+                    Month = volumes.Month,
+                    ServiceId = volumes.ServiceId,
+                    Value = volumes.Value * tariff.Value
+                };
+                Thread.Sleep(1000);
+                OnNotify.Invoke(this, $"Расчет успешно завершен в {DateTime.Now:T}");
+                return charge;
+            }
+            catch (Exception e)
+            {
+                OnException.Invoke(this, new Tuple<string, Exception>("Ошибка", e));
+                throw;
+            }
+        }
+
+        static void Notify(object sender, string text) => Console.WriteLine(text);
+        static void Exception(object sender, Tuple<string, Exception> text)
+        {
+            Console.WriteLine(text.Item1);
+            throw text.Item2;
+        }
+    }
+}
